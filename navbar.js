d9fa2cb7fae6f0fb2d2f1ba97f8135aa67426be2
RafaelDellis <Rafael.dellis@aluno.ifsp.edu.br>
2026-05-13 10:44:51 -0300

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,13 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    let pagina = window.location.pathname.split("/").pop();
+
     document.getElementById("navbar").innerHTML = `
         <div class="navbar">
 
             <div class="nav-links">
-                <a href="index.html">Início</a>
-                <a href="calculos.html">Cálculos</a>
-                <a href="resultados.html">Resultados</a>
-                <a href="dicas.html">Dicas</a>
-                <a href="estatisticas.html">Estatísticas</a>
+
+                <a href="index.html"
+                class="${pagina === 'index.html' ? 'active' : ''}">
+                Início
+                </a>
+
+                <a href="calculos.html"
+                class="${pagina === 'calculos.html' ? 'active' : ''}">
+                Cálculos
+                </a>
+
+                <a href="resultados.html"
+                class="${pagina === 'resultados.html' ? 'active' : ''}">
+                Resultados
+                </a>
+
+                <a href="dicas.html"
+                class="${pagina === 'dicas.html' ? 'active' : ''}">
+                Dicas
+                </a>
+
+                <a href="estatisticas.html"
+                class="${pagina === 'estatisticas.html' ? 'active' : ''}">
+                Estatísticas
+                </a>
+
             </div>
 
             <div class="nav-user">
